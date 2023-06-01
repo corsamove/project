c1608526a18ec80aa0e031ece9c017f39e4faa91
alyonatalantseva <alyonatalantseva@mail.ru>
2023-06-01 12:58:12 +0300

    добавила преобразование текста

--- function.py
+++ function.py
@@ -4,6 +4,11 @@ from docx.shared import Pt
 def change_text_style(files_list):
     for file_name in files_list:
         document = Document('Table/' + file_name)
-        
+        for paragraph in document.paragraphs:
+            paragraph.style.font.name = 'Times New Roman'
+            paragraph.style.font.size = Pt(14)
+            paragraph.style.paragraph_format.line_spacing = 1.5 
+        document.save('Table/'+ file_name + 'restyled.docx')
+
 files_to_change = ['1.docx', '2.docx', '3.docx', '4.docx', '5.docx']
 change_text_style(files_to_change)
