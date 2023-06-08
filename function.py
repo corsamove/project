431fd069d7c60f506207fc04b587ccba97b3ddc4
valerianekk <124786832+valerianekk@users.noreply.github.com>
2023-06-08 17:41:31 +0300

    отредактировала function

--- function.py
+++ function.py
@@ -9,6 +9,3 @@ def change_text_style(files_list):
             paragraph.style.font.size = Pt(14)
             paragraph.style.paragraph_format.line_spacing = 1.5 
         document.save(file_name + 'restyled.docx')
-
-files_to_change = ['1.docx', '2.docx', '3.docx', '4.docx', '5.docx']
-change_text_style(files_to_change)
